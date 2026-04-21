e96b7258b513b02a63e7a6a7b502636c2e8217ff
seshukalakurthi <seshubabu.kalakurthi@gmail.com>
2026-04-21 18:36:04 +0100

    added backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,8 +7,18 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "eadca2terraformdeploy"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
 }
 
 provider "azurerm" {
   features {}
+    subscription_id = var.subscription_id
+    tenant_id       = var.tenant_id
 }
